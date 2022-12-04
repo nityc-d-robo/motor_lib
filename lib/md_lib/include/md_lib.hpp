46d5602dae38defaa4cd47e5606911df2f934dd2
k-trash <d.kintou@gmail.com>
2022-12-04 23:41:43 +0900

    adjusting to usb_connect

--- lib/md_lib/include/md_lib.hpp
+++ lib/md_lib/include/md_lib.hpp
@@ -2,14 +2,27 @@
 
 #include <serial_connect/serial_connect.hpp>
 
+typedef struct MdStatus{
+	float firmware;
+	int32_t encoder_cnt;
+	bool lim_sw1, lim_sw2;
+}MdStatus;
+
 class Md{
 	public:
 		Md(SerialConnect& serial_);
-		void sendPwm(uint16_t address_, uint16_t semi_id_, bool phase_, uint16_t power_, uint16_t usb_timeout_);
-		void sendSpeed(uint16_t address_, bool phase_, uint16_t speed_, uint16_t end_);
-		void sendAngle(uint16_t address_, bool phase_, uint16_t speed_, uint16_t angle_);
-		void sendLimSw(uint16_t address_, bool phase_, uint16_t speed_, bool port_);
-
+		void sendPwm(uint16_t address_, bool phase_, uint16_t power_, uint32_t usb_timeout_);
+		void sendPwm(uint16_t address_, uint16_t semi_id_, bool phase_, uint16_t power_, uint32_t usb_timeout_);
+		void sendSpeed(uint16_t address_, bool phase_, uint16_t speed_, uint16_t end_, uint16_t timeout_, uint32_t usb_timeout_);
+		void sendSpeed(uint16_t address_, uint16_t semi_id_, bool phase_, uint16_t speed_, uint16_t end_, uint16_t timeout_, uint32_t usb_timeout_);
+		void sendAngle(uint16_t address_, bool phase_, uint16_t speed_, uint16_t angle_, uint16_t timeout_, uint32_t usb_timeout_);
+		void sendAngle(uint16_t address_, uint16_t semi_id_, bool phase_, uint16_t speed_, uint16_t angle_, uint16_t timeout_, uint32_t usb_timeout_);
+		void sendLimSw(uint16_t address_, bool phase_, uint16_t speed_, bool port_, uint32_t usb_timeout_);
+		void sendLimSw(uint16_t address_, uint16_t semi_id_, bool phase_, uint16_t speed_, bool port_, uint32_t usb_timeout_);
+		void sendInit(uint16_t address_, bool angle_reset_, uint16_t max_rpm_, uint16_t max_torque_, uint16_t gear_ratio_, uint32_t usb_timeout_);
+		void sendInit(uint16_t address_, uint16_t semi_id_, bool angle_reset_, uint16_t max_rpm_, uint16_t max_torque_, uint16_t gear_ratio_, uint32_t usb_timeout_);
+		void sendStatus(uint16_t address_, MdStatus& md_status_, uint32_t usb_timeout_);
+		void sendStatus(uint16_t address_, uint16_t semi_id_, MdStatus& md_status_, uint32_t usb_timeout_);
 	private:
 		SerialConnect* serial;
 		typedef enum Mode{
--- lib/md_lib/src/md_lib.cpp
+++ lib/md_lib/src/md_lib.cpp
@@ -2,38 +2,72 @@
 
 #include <usb_connect/usb_connect.hpp>
 
-Md::Md(SerialConnect& serial_){
-	serial = &serial_;
+Md::Md(UsbConnect& usb_){
+	usb = &usb_;
 }
 
-void Md::sendPwm(uint16_t address_, bool phase_, uint16_t power_){
-	uint8_t send_buf[7] = {0u};
+UsbStatus Md::sendPwm(uint8_t address_, bool phase_, uint16_t power_, uint32_t usb_timeout_){
+	uint8_t send_buf[10] = {0u};
 
-	send_buf[0] = 0x01;
-	send_buf[1] = (uint8_t)address_;
-	send_buf[2] = (uint8_t)(PWM << 1) | (uint8_t)phase_;
-	send_buf[3] = (uint8_t)((power_ >> 8) & 0xff);
-	send_buf[4] = (uint8_t)(power_ & 0xff);
+	send_buf[0] = address_;
+	send_buf[1] = 0u;
+	send_buf[2] = (uint8_t)PWM;
+	send_buf[3] = (uint8_t)phase_;
+	send_buf[4] = (uint8_t)((power_ >> 8) & 0xff);
+	send_buf[5] = (uint8_t)(power_ & 0xff);
 
-	serial->writeSerial(send_buf, 7);
+	return usb->writeUsb(send_buf, 10, EndPoint::EP1, usb_timeout_);
 }
 
-void Md::sendSpeed(uint16_t address_, bool phase_, uint16_t speed_, uint16_t end_){
-	uint8_t send_buf[7] = {0u};
+UsbStatus Md::sendPwm(uint8_t address_, uint8_t semi_id_, bool phase_, uint16_t power_, uint32_t usb_timeout_){
+	uint8_t send_buf[10] = {0u};
 
-	send_buf[0] = 0x01;
-	send_buf[1] = (uint8_t)address_;
-	send_buf[2] = (uint8_t)(SPEED << 1) | (uint8_t)phase_;
-	send_buf[3] = (uint8_t)((speed_ >> 8) & 0xff);
-	send_buf[4] = (uint8_t)(speed_ & 0xff);
-	send_buf[5] = (uint8_t)((end_ >> 8) & 0xff);
-	send_buf[6] = (uint8_t)(end_ & 0xff);
+	send_buf[0] = address_;
+	send_buf[1] = semi_id_;
+	send_buf[2] = (uint8_t)PWM;
+	send_buf[3] = (uint8_t)phase_;
+	send_buf[4] = (uint8_t)((power_ >> 8) & 0xff);
+	send_buf[5] = (uint8_t)(power_ & 0xff);
 
-	serial->writeSerial(send_buf, 7);
+	return usb->writeUsb(send_buf, 10, EndPoint::EP1, usb_timeout_);
 }
 
-void Md::sendAngle(uint16_t address_, bool phase_, uint16_t speed_, uint16_t angle_){
-	uint8_t send_buf[7] = {0u};
+UsbStatus Md::sendSpeed(uint8_t address_, bool phase_, uint16_t speed_, uint16_t end_, uint16_t timeout_, uint32_t usb_time_){
+	uint8_t send_buf[10] = {0u};
+
+	send_buf[0] = address_;
+	send_buf[1] = 0u;
+	send_buf[2] = (uint8_t)SPEED
+	send_buf[3] = (uint8_t)phase_;
+	send_buf[4] = (uint8_t)((speed_ >> 8) & 0xff);
+	send_buf[5] = (uint8_t)(speed_ & 0xff);
+	send_buf[6] = (uint8_t)((end_ >> 8) & 0xff);
+	send_buf[7] = (uint8_t)(end_ & 0xff);
+	send_buf[8] = (uint8_t)((timeout_ >> 8) & 0xff);
+	send_buf[9] = (uint8_t)(timeout_ & 0xff);
+
+	return usb->writeUsb(send_buf, 10, EndPoint::EP1, usb_timeout_);
+}
+
+UsbStatus Md::sendSpeed(uint8_t address_, uint8_t semi_id_, bool phase_, uint16_t speed_, uint16_t end_, uint16_t timeout_, uint32_t usb_timeout_){
+	uint8_t send_buf[10] = {0u};
+
+	send_buf[0] = address_;
+	send_buf[1] = semi_id_;
+	send_buf[2] = (uint8_t)SPEED
+	send_buf[3] = (uint8_t)phase_;
+	send_buf[4] = (uint8_t)((speed_ >> 8) & 0xff);
+	send_buf[5] = (uint8_t)(speed_ & 0xff);
+	send_buf[6] = (uint8_t)((end_ >> 8) & 0xff);
+	send_buf[7] = (uint8_t)(end_ & 0xff);
+	send_buf[8] = (uint8_t)((timeout_ >> 8) & 0xff);
+	send_buf[9] = (uint8_t)(timeout_ & 0xff);
+
+	return usb->writeUsb(send_buf, 10, EndPoint::EP1, usb_timeout_);
+}
+
+void Md::sendAngle(uint16_t address_, uint16_t speed_, int32_t angle_, uint16_t timeout_, uint32_t usb_timeout_){
+	uint8_t send_buf[8] = {0u};
 
 	send_buf[0] = 0x01;
 	send_buf[1] = (uint8_t)address_;
