ccdf1debba2acaf4f00a605a1735e23eec35d6ed
sarukiti <sarukiti1891@gmail.com>
2022-12-19 17:23:12 +0900

    サーボモーターが回るようになった

--- lib/smd_lib/src/smd_lib.cpp
+++ lib/smd_lib/src/smd_lib.cpp
@@ -11,7 +11,7 @@ void SMd::sendAngle(uint16_t address_, uint16_t mode_, uint16_t mask_, uint16_t 
 
 	send_buf[0] = 0x01;
 	send_buf[1] = (uint8_t)address_;
-	send_buf[2] = (uint8_t)(mode_ << 1) | ((uint8_t)mask_ & 0x01);
+	send_buf[2] = (uint8_t)((uint8_t)mode_ << 1) | ((uint8_t)mask_ & 0x01);
 	send_buf[3] = (uint8_t)((angle1_ >> 8) & 0xff);
 	send_buf[4] = (uint8_t)(angle1_ & 0xff);
 	send_buf[5] = (uint8_t)((angle2_ >> 8) & 0xff);
--- test/test_smd.cpp
+++ test/test_smd.cpp
@@ -1,10 +1,11 @@
 #include <motor_lib/motor_lib.hpp>
 #include "unistd.h"
 int main(void){
-    motor::serial.setSerial("/dev/ttyACM0",115200);
+    motor::serial.setSerial("/dev/ttyACM0",B115200);
     motor::serial.openSerial();
+    motor::smd.sendAngle(0x10, 2, 0, 0, 30);
+    usleep(50000);
     motor::stopAll();
     usleep(50000);
-    motor::md.sendPwm(0x01, 0, 300);
-    usleep(50000);
+    motor::smd.sendAngle(0x10, 2, 0, 30, 30);
 }
