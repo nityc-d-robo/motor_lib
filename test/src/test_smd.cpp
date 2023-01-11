4c933f54434018cf5e2d120b05ebb4b8adb1a559
d-robo-ynct <79290194+d-robo-ynct@users.noreply.github.com>
2023-01-11 19:26:13 +0900

    テストコードを追加・修正

--- test/src/test_smd.cpp
+++ test/src/test_smd.cpp
@@ -3,9 +3,9 @@
 int main(void){
     motor::serial.setSerial("/dev/ttyACM0",B115200);
     motor::serial.openSerial();
-    motor::smd.sendAngle(0x10, 2, 0, 0, 30);
-    usleep(50000);
+    motor::smd.sendAngle(0x10, 2, 0, 0);
+    usleep(2 * 1000000);
     motor::stopAll();
-    usleep(50000);
-    motor::smd.sendAngle(0x10, 2, 0, 30, 30);
+    usleep(2 * 1000000);
+    motor::smd.sendAngle(0x10, 2, 0, 30);
 }
