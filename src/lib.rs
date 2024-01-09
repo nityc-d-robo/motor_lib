04e6b66d643d9bf40ee4fd0a40429b160e833fc2
sarukiti <sarukiti1891@gmail.com>
2024-01-10 02:43:46 +0900

    WIP: 暫定ロボマス対応？

---
+++ src/lib.rs
@@ -0,0 +1,60 @@
+use rusb::{open_device_with_vid_pid, DeviceHandle, GlobalContext};
+
+pub mod md{
+    use std::time::Duration;
+    use rusb::{DeviceHandle, GlobalContext, constants::{LIBUSB_ENDPOINT_IN, LIBUSB_ENDPOINT_OUT}};
+
+    #[derive(Debug)]
+    pub struct MdStatus{
+        pub std_id: u16,
+        pub angle: i16,
+        pub speed: i16,
+        pub current: i16,
+    }
+
+    pub fn send_current(handle_: DeviceHandle<GlobalContext>, end_point_: u8, std_id_: u16, controller_id_: u8, current_: i16) -> MdStatus{
+        let send_buf: [u8; 5] = [
+            ((std_id_ >> 8) & 0xff) as u8,
+            (std_id_ & 0xff) as u8,
+            controller_id_,
+            ((current_ >> 8) & 0xff) as u8,
+            (current_ & 0xff) as u8,
+        ];
+        handle_.write_bulk(LIBUSB_ENDPOINT_OUT | end_point_, &send_buf, Duration::from_millis(5000)).unwrap();
+        
+        let mut receive_buf:[u8; 8] = [0; 8];
+        loop {
+            handle_.read_bulk(LIBUSB_ENDPOINT_IN | end_point_, &mut receive_buf, Duration::from_millis(5000)).unwrap();
+            let received_stdid = (receive_buf[0] as u16) << 8 | (receive_buf[1] as u16);
+            if received_stdid == (std_id_ + (controller_id_ as u16)){
+                return MdStatus{
+                    std_id: received_stdid,
+                    angle: ((receive_buf[2] as i16) << 8 | (receive_buf[3] as i16)),
+                    speed: ((receive_buf[4] as i16) << 8 | (receive_buf[5] as i16)),
+                    current: ((receive_buf[6] as i16) << 8 | (receive_buf[7] as i16)),
+                }
+            }
+        }
+    }
+}
+
+pub fn init_usb_handle(vendor_id: u16, product_id: u16, b_interface_number: u8) -> DeviceHandle<GlobalContext>{
+    let mut handle = open_device_with_vid_pid(vendor_id, product_id).unwrap();
+    handle.set_auto_detach_kernel_driver(true).unwrap();
+    handle.claim_interface(b_interface_number).unwrap();
+    return handle
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::md::send_current;
+
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let handle = init_usb_handle(0x483, 0x5740, 0);
+        let return_status = send_current(handle, 1, 0x200, 1, 16384);
+        println!("{:?}", return_status);
+    }
+}
