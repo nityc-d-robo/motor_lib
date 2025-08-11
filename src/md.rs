cee6bf306efbf1883a103a02b3008766987e0ff6
mituba3328 <yukitoharuto@outlook.jp>
2025-08-11 15:28:55 +0900

    sendPWM,sendSPEED delete receive_status

--- src/md.rs
+++ src/md.rs
@@ -64,7 +64,7 @@ pub fn send_pwm(
     handle: &impl HandleTrait,
     address: u8,
     power: i16,
-) -> Result<MdStatus, crate::Error> {
+) {
     let send_buf: [u8; 8] = [
         address,
         device_type::MASTER,
@@ -75,8 +75,7 @@ pub fn send_pwm(
         0,
         0,
     ];
-    handle.write_bulk(&send_buf, Duration::from_millis(5000))?;
-    return receive_status(handle, address);
+    let _ = handle.write_bulk(&send_buf, Duration::from_millis(5000));
 }
 
 /// Sends a command to set the rotation speed on the specified MD device.
@@ -107,23 +106,18 @@ pub fn send_speed(
     handle: &impl HandleTrait,
     address: u8,
     velocity: i16,
-) -> Result<MdStatus, crate::Error> {
-    if velocity == 0 {
-        send_pwm(handle, address, 0)?;
-    } else {
-        let send_buf: [u8; 8] = [
-            address,
-            device_type::MASTER,
-            mode::SPEED,
-            0,
-            ((velocity >> 8) & 0xff) as u8,
-            (velocity & 0xff) as u8,
-            0,
-            0,
-        ];
-        handle.write_bulk(&send_buf, Duration::from_millis(5000))?;
-    }
-    return receive_status(handle, address);
+) {
+    let send_buf: [u8; 8] = [
+        address,
+        device_type::MASTER,
+        mode::SPEED,
+        0,
+        ((velocity >> 8) & 0xff) as u8,
+        (velocity & 0xff) as u8,
+        0,
+        0,
+    ];
+    let _ = handle.write_bulk(&send_buf, Duration::from_millis(5000));
 }
 
 /// Sends a command to set the angle on the specified MD device.
